3ef3cfc72ee4f912b8a0902efb48ee93faf35801
Anton Rozhnev <a.rojnev@elama.ru>
2016-09-28 13:11:59 +0300

    recreate custom fields client url

--- EventServiceLib/EventServiceValues.php
+++ EventServiceLib/EventServiceValues.php
@@ -22,5 +22,5 @@ class EventServiceValues
 
     const CUSTOM_FIELD_VALUE_STATUS = 'C';
     const CUSTOM_FIELD_VALUE_STATUS_COMMENT = 'R';
-    const CUSTOM_FIELD_VALUE_CLIENT_URL = 'X';
+    const CUSTOM_FIELD_VALUE_CLIENT_URL = '9';
 }
