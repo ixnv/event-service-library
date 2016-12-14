e7a6fca6819f88478fde5755b7e116563d8996ab
Horhe <a.tretiyakov@elama.ru>
2016-12-14 15:06:50 +0300

    Update contact - message fix

--- EventServiceLib/Message/UpdateAmoCrmContactMessage.php
+++ EventServiceLib/Message/UpdateAmoCrmContactMessage.php
@@ -5,5 +5,11 @@ namespace EventServiceLib\Message;
 
 class UpdateAmoCrmContactMessage extends RegistrationMessage
 {
-
+    /**
+     * @return string
+     */
+    function getEventIdentity()
+    {
+        return 'updateContact';
+    }
 }
