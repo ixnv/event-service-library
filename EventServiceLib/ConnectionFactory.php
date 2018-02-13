8a10230716dece97a26c0572d90010418ef5d1db
Anton Rozhnev <a.rojnev@elama.ru>
2018-02-13 14:15:07 +0300

    GS-1329 refact connectionFactory, add check connection in QueueManager

--- EventServiceLib/ConnectionFactory.php
+++ EventServiceLib/ConnectionFactory.php
@@ -4,22 +4,13 @@
 namespace EventServiceLib;
 
 
+use EventServiceLib\Exceptions\EventServiceException;
 use Exception;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Exception\AMQPExceptionInterface;
-use Psr\Log\LoggerInterface;
 
 class ConnectionFactory
 {
-    /** @var  LoggerInterface */
-    private $logger;
-
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
-
-
     /**
      * @param string $host
      * @param int $port
@@ -27,7 +18,7 @@ class ConnectionFactory
      * @param string $password
      * @param string $vHost
      *
-     * @return bool|AMQPStreamConnection
+     * @return AMQPStreamConnection
      * @throws Exception
      */
     public function createAMQPConnection($host, $port, $user, $password, $vHost)
@@ -36,7 +27,6 @@ class ConnectionFactory
             $connection = new AMQPStreamConnection($host, $port, $user, $password, $vHost);
         } catch (Exception $e) {
             $this->handleAMQPException($e);
-            $connection = false;
         }
 
         return $connection;
@@ -52,7 +42,8 @@ class ConnectionFactory
         if (!$e instanceof AMQPExceptionInterface) {
             throw $e;
         }
-        $this->logger->critical("AMQPException caught!", [$e]);
+
+        throw new EventServiceException("AMQPException caught!");
     }
 
 }
