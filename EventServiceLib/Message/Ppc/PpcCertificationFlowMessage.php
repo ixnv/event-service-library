5d968fb9cf14a64ad280a54d63c0e162324aa935
Fedchishina <fedchishina.victoria@gmail.com>
2019-04-18 12:46:36 +0300

    добавлено сообщение для flow сертификации пользователей PPC

---
+++ EventServiceLib/Message/Ppc/PpcCertificationFlowMessage.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace EventServiceLib\Message\Ppc;
+
+use EventServiceLib\Message\Traits\ArrayEmailTrait;
+
+class PpcCertificationFlowMessage extends AbstractPpcMessage
+{
+    use ArrayEmailTrait;
+
+    private $stage;
+    private $numAttempt;
+    private $typeCertification;
+
+    public function getEventIdentity()
+    {
+        return 'ppcCourseSubscription';
+    }
+
+    public function isValid()
+    {
+        return !$this->hasEmpty([
+            $this->stage,
+            $this->numAttempt,
+            $this->typeCertification
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getStage()
+    {
+        return $this->stage;
+    }
+
+    /**
+     * @param string $stage
+     *
+     * @return PpcCertificationFlowMessage
+     */
+    public function setStage($stage)
+    {
+        $this->stage = $stage;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumAttempt()
+    {
+        return $this->numAttempt;
+    }
+
+    /**
+     * @param int $numAttempt
+     *
+     * @return PpcCertificationFlowMessage
+     */
+    public function setNumAttempt($numAttempt)
+    {
+        $this->numAttempt = $numAttempt;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTypeCertification()
+    {
+        return $this->typeCertification;
+    }
+
+    /**
+     * @param string $typeCertification
+     *
+     * @return PpcCertificationFlowMessage
+     */
+    public function setTypeCertification($typeCertification)
+    {
+        $this->typeCertification = $typeCertification;
+
+        return $this;
+    }
+
+}
