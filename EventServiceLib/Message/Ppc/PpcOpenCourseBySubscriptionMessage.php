455663199d94969b3c80900d47e8e3ee58bcb5fb
Fedchishina <fedchishina.victoria@gmail.com>
2019-04-19 15:12:38 +0300

    добавлен тип сообщения для открытия курса по записи

---
+++ EventServiceLib/Message/Ppc/PpcOpenCourseBySubscriptionMessage.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace EventServiceLib\Message\Ppc;
+
+
+class PpcOpenCourseBySubscriptionMessage extends AbstractPpcMessage
+{
+    protected $courseId;
+    protected $courseName;
+    protected $courseUrl;
+    protected $emailMarkup;
+
+    public function getEventIdentity()
+    {
+        return 'ppcOpenCourseBySubscription';
+    }
+
+    public function isValid()
+    {
+        return !$this->hasEmpty([
+            $this->courseId,
+            $this->courseName,
+            $this->courseUrl,
+            $this->emailMarkup
+        ]);
+    }
+
+    /**
+     * @return int
+     */
+    public function getCourseId()
+    {
+        return $this->courseId;
+    }
+
+    /**
+     * @param int $courseId
+     *
+     * @return PpcOpenCourseBySubscriptionMessage
+     */
+    public function setCourseId($courseId)
+    {
+        $this->courseId = $courseId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCourseName()
+    {
+        return $this->courseName;
+    }
+
+    /**
+     * @param string $courseName
+     *
+     * @return PpcOpenCourseBySubscriptionMessage
+     */
+    public function setCourseName($courseName)
+    {
+        $this->courseName = $courseName;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCourseUrl()
+    {
+        return $this->courseUrl;
+    }
+
+    /**
+     * @param string $courseUrl
+     *
+     * @return PpcOpenCourseBySubscriptionMessage
+     */
+    public function setCourseUrl($courseUrl)
+    {
+        $this->courseUrl = $courseUrl;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmailMarkup()
+    {
+        return $this->emailMarkup;
+    }
+
+    /**
+     * @param mixed $emailMarkup
+     *
+     * @return PpcOpenCourseBySubscriptionMessage
+     */
+    public function setEmailMarkup($emailMarkup)
+    {
+        $this->emailMarkup = $emailMarkup;
+
+        return $this;
+    }
+
+}
