07d93c82590ba43d3e183fdcafec4f4d6bdbdd57
Anton Rojnev <a.rojnev@elama.ru>
2019-08-13 13:51:19 +0300

    feat: GS-3081 Worksection
    - удалено сообщение MailchimpUsersMessage
    - добавлено сообщение WorksectionCreateH2SFinishedMessage
    
    МЕТА ДАННЫЕ: https://jira.elama.ru/browse/GS-3081

---
+++ EventServiceLib/Message/Utm/WorksectionCreateH2sFinishedMessage.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace EventServiceLib\Message\Utm;
+
+use EventServiceLib\Message\AbstractMessage;
+
+class WorksectionCreateH2SFinishedMessage extends AbstractMessage
+{
+
+    protected $elamaId;
+    protected $tag;
+    protected $campaignType;
+
+    /**
+     * @return string
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
+
+    /**
+     * @param string $tag
+     * @return WorksectionCreateH2SFinishedMessage
+     */
+    public function setTag($tag)
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCampaignType()
+    {
+        return $this->campaignType;
+    }
+
+    /**
+     * @param string $campaignType
+     * @return WorksectionCreateH2SFinishedMessage
+     */
+    public function setCampaignType($campaignType)
+    {
+        $this->campaignType = $campaignType;
+
+        return $this;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getElamaId()
+    {
+        return $this->elamaId;
+    }
+
+    /**
+     * @param int $elamaId
+     *
+     * @return $this
+     */
+    public function setElamaId($elamaId)
+    {
+        $this->elamaId = $elamaId;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        return !$this->hasEmpty([$this->elamaId, $this->tag, $this->campaignType]);
+    }
+
+    /**
+     * @return string
+     */
+    function getEventIdentity()
+    {
+        return 'worksectionCreateH2SFinished';
+    }
+
+}
