2a0890e7bb14877d1f8f4ab93b73243fe339b269
a.rojnev <a.rojnev@elama.ru>
2019-10-29 16:35:21 +0300

    feat: ID-188 новое cообщение GetReportTrial
    МЕТА ДАННЫЕ: https://jira.elama.ru/browse/ID-188

---
+++ EventServiceLib/Message/Report/GetReportTrial.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace EventServiceLib\Message\Report;
+
+use EventServiceLib\Message\AbstractMessage;
+
+class GetReportTrial extends AbstractMessage
+{
+
+    protected $elamaId;
+
+    /**
+     * @return int
+     */
+    public function getElamaId()
+    {
+        return $this->elamaId;
+    }
+
+    /**
+     * @param int $elamaId
+     * @return GetReportTrial
+     */
+    public function setElamaId($elamaId)
+    {
+        $this->elamaId = $elamaId;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    function getEventIdentity()
+    {
+        return 'getReportTrial';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        return !$this->hasEmpty([
+            $this->elamaId,
+        ]);
+    }
+
+}
