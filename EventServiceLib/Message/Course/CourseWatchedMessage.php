b0b4496bf4dc9fe3e43bd762ac8aad0a6a4433b6
a.rojnev <a.rojnev@elama.ru>
2019-11-18 17:48:52 +0300

    feat: ID-198 новое cообщение CourseWatched
    - событие при просмотре лекции на курсах
    
    МЕТА ДАННЫЕ: https://jira.elama.ru/browse/ID-198

---
+++ EventServiceLib/Message/Course/CourseWatchedMessage.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace EventServiceLib\Message\Ppc;
+
+use EventServiceLib\Message\AbstractMessage;
+use EventServiceLib\Message\Traits\ArrayEmailTrait;
+
+class CourseWatchedMessage extends AbstractMessage
+{
+    use ArrayEmailTrait;
+
+    protected $courseTag;
+
+    public function getEventIdentity()
+    {
+        return 'courseWatched';
+    }
+
+    public function isValid()
+    {
+        return !$this->hasEmpty([
+            $this->email,
+            $this->courseTag
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getCourseTag()
+    {
+        return $this->courseTag;
+    }
+
+    /**
+     * @param string $courseTag
+     * @return CourseWatchedMessage
+     */
+    public function setCourseTag($courseTag)
+    {
+        $this->courseTag = $courseTag;
+
+        return $this;
+    }
+
+}
