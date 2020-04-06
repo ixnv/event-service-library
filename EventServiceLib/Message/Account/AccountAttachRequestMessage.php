1a7b90eadf18983887a2d897e22fe70368394c1e
anton <a.rojnev@elama.ru>
2020-04-06 14:52:16 +0300

    feat: ID-300 Новое сообщение
    Заявка на добавление аккаунта (при ручной модерации)
    
    МЕТА ДАННЫЕ: https://jira.elama.ru/browse/ID-300

---
+++ EventServiceLib/Message/Account/AccountAttachRequestMessage.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace EventServiceLib\Message\Account;
+
+use EventServiceLib\Message\AbstractMessage;
+
+class AccountAttachRequestMessage extends AbstractMessage
+{
+    protected $elamaId;
+    protected $advSystem;
+    protected $requestStatus;
+
+    /**
+     * @return int
+     */
+    public function getElamaId()
+    {
+        return $this->elamaId;
+    }
+
+    /**
+     * @param int $elamaId
+     * @return AccountAttachRequestMessage
+     */
+    public function setElamaId($elamaId)
+    {
+        $this->elamaId = $elamaId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdvSystem()
+    {
+        return $this->advSystem;
+    }
+
+    /**
+     * @param string $advSystem
+     * @return AccountAttachRequestMessage
+     */
+    public function setAdvSystem($advSystem)
+    {
+        $this->advSystem = $advSystem;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRequestStatus()
+    {
+        return $this->requestStatus;
+    }
+
+    /**
+     * @param string $requestStatus
+     * @return AccountAttachRequestMessage
+     */
+    public function setRequestStatus($requestStatus)
+    {
+        $this->requestStatus = $requestStatus;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    function getEventIdentity()
+    {
+        return 'accountAttachRequest';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        return !$this->hasEmpty(
+            [
+                $this->elamaId,
+                $this->requestStatus,
+                $this->advSystem
+            ]
+        );
+    }
+}
