c34b979faae1ba0b3dafa46e6a98bedb5df3a5e9
anton <a.rojnev@elama.ru>
2020-04-15 12:21:01 +0300

    feat: ID-304 сообщение изменения юнита
    МЕТА ДАННЫЕ: https://jira.elama.ru/browse/ID-304

---
+++ EventServiceLib/Message/ChangeUserUnitTypeMessage.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace EventServiceLib\Message;
+
+class ChangeUserUnitTypeMessage extends AbstractMessage
+{
+
+    const UNIT_TYPE_SELF_SERVICE = 'self_service';
+    const UNIT_TYPE_AGENCY = 'agency';
+    const UNIT_TYPE_AGENCY_CLIENT = 'agency_client';
+
+    protected $elamaId;
+    protected $email;
+    protected $unitType;
+
+    /**
+     * @return string
+     */
+    function getEventIdentity()
+    {
+        return 'changeUserUnitType';
+    }
+
+    /**
+     * @return int
+     */
+    public function getElamaId()
+    {
+        return $this->elamaId;
+    }
+
+    /**
+     * @param int $elamaId
+     *
+     * @return ChangeUserUnitTypeMessage
+     */
+    public function setElamaId($elamaId)
+    {
+        $this->elamaId = $elamaId;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return ChangeUserUnitTypeMessage
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUnitType()
+    {
+        return $this->unitType;
+    }
+
+    /**
+     * @param string $unitType
+     * @return ChangeUserUnitTypeMessage
+     */
+    public function setUnitType($unitType)
+    {
+        $this->unitType = $unitType;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        return !$this->hasEmpty(
+                [
+                    $this->elamaId,
+                ]
+            )
+            && (!$this->unitType || in_array(
+                    $this->unitType,
+                    [
+                        self::UNIT_TYPE_AGENCY,
+                        self::UNIT_TYPE_AGENCY_CLIENT,
+                        self::UNIT_TYPE_SELF_SERVICE,
+                    ]
+                ));
+    }
+
+}
