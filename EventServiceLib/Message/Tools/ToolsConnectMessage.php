3361dbba528a57809c23a968a5e03413662b77e0
anton <a.rojnev@elama.ru>
2020-05-25 10:43:20 +0300

    feat: ID-297 Единое сообщение для подключения инструментов
    МЕТА ДАННЫЕ: https://jira.elama.ru/browse/ID-297

---
+++ EventServiceLib/Message/Tools/ToolsConnectMessage.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace EventServiceLib\Message\Tools;
+
+use EventServiceLib\Message\AbstractMessage;
+
+/** Пользователь подключил инструмент */
+class ToolsConnectMessage extends AbstractMessage
+{
+    const EVENT_IDENTITY = 'toolsConnect';
+
+    private $elamaId;
+    private $email;
+    private $toolName;
+    private $userLocale;
+
+    /**
+     * @return int
+     */
+    public function getElamaId()
+    {
+        return $this->elamaId;
+    }
+
+    /**
+     * @param int $elamaId
+     * @return ToolsConnectMessage
+     */
+    public function setElamaId($elamaId)
+    {
+        $this->elamaId = $elamaId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return ToolsConnectMessage
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToolName()
+    {
+        return $this->toolName;
+    }
+
+    /**
+     * @param string $toolName
+     * @return ToolsConnectMessage
+     */
+    public function setToolName($toolName)
+    {
+        $this->toolName = $toolName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserLocale()
+    {
+        return $this->userLocale;
+    }
+
+    /**
+     * @param string $userLocale
+     * @return ToolsConnectMessage
+     */
+    public function setUserLocale($userLocale)
+    {
+        $this->userLocale = $userLocale;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        return
+            !$this->hasEmpty([$this->toolName]) ||
+            !$this->hasEmpty(
+                [
+                    $this->email,
+                    $this->userLocale
+                ]
+            ) ||
+            !$this->hasEmpty([$this->elamaId]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventIdentity()
+    {
+        return self::EVENT_IDENTITY;
+    }
+}
