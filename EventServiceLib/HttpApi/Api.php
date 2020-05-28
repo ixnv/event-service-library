39eb814d35840a04da4492f86551424d5e54062f
anton <a.rojnev@elama.ru>
2020-05-28 14:59:01 +0300

    refactor: ID-329 Отправка сообщений по апи
    МЕТА ДАННЫЕ: https://jira.elama.ru/browse/ID-329

---
+++ EventServiceLib/HttpApi/Api.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace EventServiceLib\HttpApi;
+
+use EventServiceLib\Exceptions\EventServiceApiException;
+
+class Api
+{
+    private $ESApiUrl;
+    private $initiatorServiceName;
+
+    /**
+     * @param string $ESApiUrl
+     * @param string $initiatorServiceName
+     */
+    public function __construct($ESApiUrl, $initiatorServiceName)
+    {
+        $this->ESApiUrl = $ESApiUrl;
+        $this->initiatorServiceName = $initiatorServiceName;
+    }
+
+    /**
+     * @param string $link
+     * @param string $encodedMessage
+     */
+    public function callApi($link, $encodedMessage = null)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'ESlib-API-client/1.0');
+        curl_setopt($curl, CURLOPT_URL, $link);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+
+        if ($encodedMessage) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $encodedMessage);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        }
+
+        $out = curl_exec($curl);
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        if ($code >= 200 && $code < 300) {
+            return $out;
+        }
+
+        throw new EventServiceApiException(
+            $code,
+            $out,
+            'Event-service API is not available'
+        );
+    }
+
+    /**
+     * @return bool
+     */
+    public function ping()
+    {
+        try {
+            $this->callApi(rtrim($this->ESApiUrl, '/') . '/ping');
+        } catch (\Exception $exception) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function sendMessage($encodedMessage)
+    {
+        return $this->callApi(rtrim($this->ESApiUrl, '/') . '/message', $encodedMessage);
+    }
+}
---
+++ EventServiceLib/HttpApi/EventDispatcher.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace EventServiceLib\HttpApi;
+
+use EventServiceLib\EventDispatcherInterface;
+use EventServiceLib\EventServiceValues;
+use EventServiceLib\Exceptions\EventServiceApiException;
+use EventServiceLib\Exceptions\EventServiceException;
+use EventServiceLib\Message\AbstractMessage;
+use EventServiceLib\Message\ProjectSpecificMessageInterface;
+
+class EventDispatcher implements EventDispatcherInterface
+{
+    private $ESApi;
+
+    public function __construct($ESApiUrl, $initiatorServiceName)
+    {
+        $this->ESApi = new Api($ESApiUrl, $initiatorServiceName);
+    }
+
+    /**
+     * @param AbstractMessage $message
+     *
+     * @return bool
+     * @throws EventServiceException
+     * @throws EventServiceApiException
+     */
+    public function dispatchMessage(AbstractMessage $message)
+    {
+        if (!$message->isValid()) {
+            return false;
+        }
+
+        if (!$this->connectionIsAvailable()) {
+            throw new EventServiceException('Event-service API is not available');
+        }
+
+        $dispatcherMessage = [
+            'version' => EventServiceValues::VERSION,
+            'messageClass' => get_class($message),
+            'type' => $message->getEventIdentity(),
+            'fields' => $message->toArray(),
+        ];
+
+        if ($message instanceof ProjectSpecificMessageInterface) {
+            $dispatcherMessage['project'] = $message->getProjectIdentity();
+        }
+
+        $dispatcherMessage = self::encodeDispatcherMessage($dispatcherMessage);
+
+        $this->ESApi->callApi($dispatcherMessage);
+
+        return true;
+    }
+
+    /**
+     * @param array $dispatcherMessage
+     *
+     * @return string
+     */
+    public static function encodeDispatcherMessage(array $dispatcherMessage)
+    {
+        return json_encode($dispatcherMessage);
+    }
+
+    /**
+     * @param string $dispatcherMessage
+     *
+     * @return array
+     */
+    public static function decodeDispatcherMessage($dispatcherMessage)
+    {
+        return json_decode($dispatcherMessage, true);
+    }
+
+    /**
+     * @return bool
+     */
+    public function connectionIsAvailable()
+    {
+        return true;
+    }
+}
